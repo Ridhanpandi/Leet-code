b66d4083cc397c6fe0b554c43fe9140e65a21116
Ridhanpandi <ridhan7788@gmail.com>
2026-07-08 20:48:27 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.java
@@ -0,0 +1,16 @@
+// Last updated: 08/07/2026, 20:48:27
+class RecentCounter {
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.add(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
