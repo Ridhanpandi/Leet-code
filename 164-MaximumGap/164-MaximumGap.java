a9537654d67663e4bafdfc68bec1ffdde6a7e5d1
Ridhanpandi <ridhan7788@gmail.com>
2026-07-08 20:50:03 +0530

    Create 164-MaximumGap

---
+++ 164-MaximumGap/164-MaximumGap.java
@@ -0,0 +1,16 @@
+// Last updated: 08/07/2026, 20:50:02
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = nums.length - 1; i > 0; i--) {
+                int sub = nums[i] - nums[i - 1];
+                max = Math.max(max, sub);
+        }
+        return max;
+
+    }
+}
