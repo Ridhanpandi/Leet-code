d0d57167e318f95a5374c7a82052ed1ab4392bb4
Ridhanpandi <ridhan7788@gmail.com>
2026-07-08 20:49:45 +0530

    Create 185-DepartmentTopThreeSalaries

---
+++ 185-DepartmentTopThreeSalaries/185-DepartmentTopThreeSalaries.sql
@@ -0,0 +1,20 @@
+-- Last updated: 08/07/2026, 20:49:44
+# Write your MySQL query statement below
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT
+        name,
+        salary,
+        departmentId,
+        DENSE_RANK() OVER (
+            PARTITION BY departmentId
+            ORDER BY salary DESC
+        ) AS rnk
+    FROM Employee
+) e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.rnk <= 3;
