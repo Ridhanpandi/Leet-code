939dbfa12886813ecf76d2cd167c5139b68c58e3
Ridhanpandi <ridhan7788@gmail.com>
2026-07-08 20:49:33 +0530

    Create 239-SlidingWindowMaximum

---
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -0,0 +1,30 @@
+// Last updated: 08/07/2026, 20:49:33
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>(); // stores indices
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove indices outside the window
+            if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            // Remove smaller elements from the back
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.addLast(i);
+
+            // Start recording results once first window is complete
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+}
