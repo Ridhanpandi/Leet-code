fbc8b8288faf2f0043607d45898981bcd1294e36
Ridhanpandi <ridhan7788@gmail.com>
2026-07-31 08:53:01 +0530

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,11 @@
+-- Last updated: 31/07/2026, 08:53:00
+SELECT DISTINCT
+    u.name,
+    IFNULL(
+        SUM(t.distance) OVER (PARTITION BY u.id),
+        0
+    ) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides t
+    ON u.id = t.user_id
+ORDER BY travelled_distance DESC, u.name ASC;
