d15a3935d6a7c198e7b508c0753d4c56b4456bb1
Ridhanpandi <ridhan7788@gmail.com>
2026-08-06 09:59:57 +0530

    Create 240-Searcha2DMatrixII

---
+++ 240-Searcha2DMatrixII/version/python/240-Searcha2DMatrixII_20260806_095955.py
@@ -0,0 +1,24 @@
+# Last updated: 06/08/2026, 09:59:55
+1class Solution(object):
+2    def searchMatrix(self, matrix, target):
+3        """:type matrix: List[List[int]]
+4        :type target: int
+5        :rtype: bool
+6        """
+7        if not matrix or not matrix[0]:
+8            return False
+9        
+10        m, n = len(matrix), len(matrix[0])
+11        row, col = 0, n - 1
+12        
+13        while row < m and col >= 0:
+14            current = matrix[row][col]
+15            if current == target:
+16                return True
+17            elif current > target:
+18                col -= 1
+19            else:
+20                row += 1
+21                
+22        return False
+23        
