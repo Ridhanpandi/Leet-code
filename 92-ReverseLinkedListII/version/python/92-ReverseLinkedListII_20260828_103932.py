4ba5719478a1882148b2449e045a95a8186e4480
Ridhanpandi <ridhan7788@gmail.com>
2026-08-28 10:39:32 +0530

    Create 92-ReverseLinkedListII

---
+++ 92-ReverseLinkedListII/version/python/92-ReverseLinkedListII_20260828_103932.py
@@ -0,0 +1,16 @@
+# Last updated: 28/08/2026, 10:39:32
+1class Solution(object):
+2    def deleteDuplicates(self, head):
+3        """
+4        :type head: Optional[ListNode]
+5        :rtype: Optional[ListNode]
+6        """
+7        current = head
+8        
+9        while current and current.next:
+10            if current.val == current.next.val:
+11                current.next = current.next.next
+12            else:
+13                current = current.next
+14                
+15        return head
